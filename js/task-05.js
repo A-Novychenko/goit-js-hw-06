6b364eb0efae40a2f07b7318f23b64b1939cb6b4
Aleksandr Novychenko <A.Novychenko@outlook.com>
2022-12-03 21:56:19 +0200

    completes task 5

--- js/task-05.js
+++ js/task-05.js
@@ -1,7 +1,10 @@
-// Завдання 5
-// Напиши скрипт, який під час набору тексту в інпуті input#name-input (подія input),
-// підставляє його поточне значення в span#name - output.Якщо інпут порожній,
-//     у спані повинен відображатися рядок "Anonymous".
+const inputRef = document.querySelector("#name-input");
+const outputRef = document.querySelector("#name-output");
+outputRef.textContent = "Anonymous";
 
-// <input type="text" id="name-input" placeholder="Please enter your name" />
-// <h1>Hello, <span id="name-output">Anonymous</span>!</h1>
+inputRef.addEventListener("input", coulDontThinkOfName);
+
+function coulDontThinkOfName(evt) {
+  outputRef.textContent =
+    inputRef.value !== "" ? evt.currentTarget.value : "Anonymous";
+}
