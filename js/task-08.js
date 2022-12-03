dbe85ce4bffcdd3c3f543d0e436947fa6d3adce8
Aleksandr Novychenko <A.Novychenko@outlook.com>
2022-12-03 23:51:22 +0200

    completes task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const formRef = document.querySelector(".login-form");
 
+formRef.addEventListener("submit", handleSubmit);
+
+function handleSubmit(evt) {
+  evt.preventDefault();
+
+  const {email, password} = evt.currentTarget;
+  if (email.value === "" || password.value === "") {
+    alert("Всі поля повинні бути заповнені !");
+
+    return;
+  }
+
+  const data = {
+    email: email.value,
+    password: password.value,
+  };
+
+  console.log(data);
+  evt.currentTarget.reset();
+}
