c6b9f48682470a253c17230bdcbe3de0084997d9
Aleksandr Novychenko <A.Novychenko@outlook.com>
2022-12-04 00:00:16 +0200

    completes task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorBtn = document.querySelector(".change-color");
+const colorValue = document.querySelector(".color");
+
+changeColorBtn.addEventListener("click", handleclickChangeColor);
+
+function handleclickChangeColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+  colorValue.textContent = getRandomHexColor();
+}
